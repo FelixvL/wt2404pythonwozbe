2f28bb84f19e1f479c158aacb4018e2696cbd669
Lon Lrn <lonlrn@DESKTOP-UNQVNT2.2e.lan>
2024-04-17 13:13:33 +0200

    eindstaat

--- testen/testNJ.py
+++ testen/testNJ.py
@@ -4,6 +4,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import functietotestNJ
 
 def proberen(self):
+    foutmelding = "dit is geen geldige naam"
+    foutmelding2 = "voer een naam in"
+    # self.assertEqual("emma", functietotestNJ.naamcontrole("emma"))
+    # self.assertEqual(foutmelding, functietotestNJ.naamcontrole(""))
     self.assertEqual("e", functietotestNJ.naamcontrole("emma17"))
+    # self.assertEqual(foutmelding, functietotestNJ.naamcontrole("emma17"))
+    # self.assertEqual(foutmelding, functietotestNJ.naamcontrole("emma18"))
+    # self.assertEqual(foutmelding, functietotestNJ.naamcontrole("''emma18'!@#'"))
+    # self.assertEqual(foutmelding2, functietotestNJ.naamcontrole(None))
     print("SUCCES")
     return "proberen2"
